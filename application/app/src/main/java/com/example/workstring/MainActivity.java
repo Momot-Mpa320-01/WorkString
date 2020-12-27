688cc3745049d666878c6b9864c21cf746ef9761
Momot-Mpa320-01 <76274786+Momot-Mpa320-01@users.noreply.github.com>
2020-12-27 10:04:39 +0200

    Edit MainActivity.java and edit main_activity.xml

--- application/app/src/main/java/com/example/workstring/MainActivity.java
+++ application/app/src/main/java/com/example/workstring/MainActivity.java
@@ -3,16 +3,36 @@ package com.example.workstring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tv;
+    private TextView text1,text2;
+    private Button gen;
+    private String str;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.text1);
-        tv.setText("Всё, что нам нужно – это "+Pharser.PhraserGen()+".");
+        init();
+        str = Pharser.PhraserGen();
+        text1.setText("Всё, что нам нужно – это "+str+".");
+        text2.setText(str.substring(0, 1).toUpperCase() + str.substring(1) + "– это всё, что нам нужно.");
+        gen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str = Pharser.PhraserGen();
+                text1.setText("Всё, что нам нужно – это "+str+".");
+                text2.setText(str.substring(0, 1).toUpperCase() + str.substring(1) + "– это всё, что нам нужно.");
+            }
+        });
+    }
+
+    private void init(){
+        text1 = findViewById(R.id.text1);
+        text2 = findViewById(R.id.text2);
+        gen = findViewById(R.id.gen);
 
     }
 }
