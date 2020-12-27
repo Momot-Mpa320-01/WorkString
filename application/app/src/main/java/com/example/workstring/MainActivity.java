1b8973b4f7b391092887f591e1d22d76ae8e7e81
Momot-Mpa320-01 <76274786+Momot-Mpa320-01@users.noreply.github.com>
2020-12-27 09:52:14 +0200

    Edit activity_main.xml

--- application/app/src/main/java/com/example/workstring/MainActivity.java
+++ application/app/src/main/java/com/example/workstring/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.workstring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.text1);
+        tv.setText("Всё, что нам нужно – это "+Pharser.PhraserGen()+".");
+
     }
 }
